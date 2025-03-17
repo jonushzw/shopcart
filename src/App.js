cc2ab5d632caf60ec578d8d2692031ee189efdcd
jonushzw <jonushzw@gmail.com>
2025-03-17 20:38:17 +0800

    Made simple home page for navigation to assignments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './globals.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import DesignPage from './pages/design';
+import ShopPage from './pages/shop';
+import styles from './styles/home.module.css';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <Link to="/design">Go to Design Page</Link>
+    <div className={styles.home}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Technical Assessment</h1>
+        <div>
+        <p className={styles.subtitle}>Links to Both Assignments Below</p>
+        <Link to="/design" className={styles.ctaButton}>Assignment 1: Design</Link>
+        <Link to="/shop" className={styles.ctaButton}>Assignment 2: Shopping Cart</Link>
+        </div>
+      </header>
     </div>
   );
 }
@@ -17,6 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/design" element={<DesignPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </Router>
   );
